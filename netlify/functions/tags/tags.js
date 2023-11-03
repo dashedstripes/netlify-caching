cd0f426ce87928bd966cbc96cdcfdbfa13d9f8d3
Adam Gray <adam.gray@netlify.com>
2023-11-03 10:19:58 -0500

    test fix

--- netlify/functions/tags/tags.js
+++ netlify/functions/tags/tags.js
@@ -6,7 +6,7 @@ const handler = async (event) => {
       body: `<html><body><h1>${Date.now()}</h1></body></html>`,
       headers: { 
         "content-type": "text/html",
-        "Netlify-CDN-Cache-Control": "public max-age=0 stale-while-revalidate=10000",
+        "Netlify-CDN-Cache-Control": "public, max-age=0, stale-while-revalidate=10000",
       },
     }
   } catch (error) {
